135fe0bc505df73e543b1ea1656a64022454345a
Anton <tepa-86@mail.ru>
2022-10-13 17:16:06 +0300

    Add Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+
+ввод элеменетов через консоль (через запятую. использовать метод string.Split())*/
+
+Console.Write("Введите целые числа через пробел: ");
+int[] myArray = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write(myArray[i]);
+    if (i != myArray.Length - 1)
+        Console.Write(", ");
+}
